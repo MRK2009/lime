15bbc0d911d06a8f1816950d55d8dc13b5d6a50f
Josh Tynjala <joshtynjala@bowlerhat.dev>
2026-01-14 10:24:14 -0800

    AudioBuffer: comment explaining the try/catch in __getCodec around bytes.getString()
    Followup to commit 9b2be25b31c1852a4cf297f3aa02395c167ec622

--- src/lime/media/AudioBuffer.hx
+++ src/lime/media/AudioBuffer.hx
@@ -424,7 +424,12 @@ class AudioBuffer
 		{
 			signature = bytes.getString(0, 4);
 		}
-		catch (e:Dynamic) {}
+		catch (e:Dynamic)
+		{
+			// if the bytes don't represent a valid UTF-8 string, getString()
+			// may throw an exception. in that case, we expect to end up in
+			// the default switch case below where it tries to detect MP3.
+		}
 
 		switch (signature)
 		{
